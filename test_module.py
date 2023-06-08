13815d642a99b54e2f33ffcb4f681f6be631870e
Дмитрий Большаков <dimaqwerty121@yandex.ru>
2023-06-08 18:47:06 +0300

    according to PEP8

--- test_module.py
+++ test_module.py
@@ -1,5 +1,7 @@
 import os
 
+from main import choice_char_class, start_training
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
@@ -9,5 +11,17 @@ files_list = ['main.py', 'readme.md']
 
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
+        assert filename in dir_files, f'Файл `{filename}` не найден'
 
+
+def main():
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class = choice_char_class()
+    print(start_training(char_name, char_class))
+    main()
